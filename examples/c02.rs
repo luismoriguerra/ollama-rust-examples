247b576c11db2b33bbef6821510f01f68413385d
Luis Mori Guerra <luismorith@gmail.com>
2024-04-22 09:38:14 -0500

    c02 adding context memory a list of prompts
    Signed-off-by: Luis Mori Guerra <luismorith@gmail.com>

--- examples/c02.rs
+++ examples/c02.rs
@@ -1,27 +1,42 @@
-use ollama_rs::{generation::completion::request::GenerationRequest, Ollama};
-use xp_ollama::{
-    consts::{DEFAUL_SYSTEM_MOCK, MODEL},
-    gen::gen_stream_print,
-    Result
+use ollama_rs::{
+    generation::completion::{request::GenerationRequest, GenerationContext},
+    Ollama
 };
+use simple_fs::{ensure_file_dir, save_json};
+use xp_ollama::{consts::MODEL, gen::gen_stream_print, Result};
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let ollama = Ollama::default();
 
-    let model = MODEL.to_string();
+    let prompts = &[
+        "why the sky is red  ? (be concise) ",
+        "what was my first question ?"
+    ];
 
-    let prompt = "what is the best programming language? ".to_string();
+    let mut prev_prompt_ctx: Option<GenerationContext> = None;
 
-    let gen_req = GenerationRequest::new(model, prompt)
-        .system(DEFAUL_SYSTEM_MOCK.to_string());
+    for prompt in prompts {
+        println!("Prompt: {}", prompt);
 
-    //  simple response generation
-    // let res = ollama.generate(gen_req).await?;
-    // println!("->> res {}", res.response);
+        let mut gen_req =
+            GenerationRequest::new(MODEL.to_string(), prompt.to_string());
 
-    //  stream response generation
-    gen_stream_print(&ollama, gen_req).await?;
+        // in the 2nd step , we need the context of the previous prompt
+        if let Some(prev_prompt_ctx) = prev_prompt_ctx.take() {
+            let ctx_file_path = "examples/c02_ctx.json";
+            ensure_file_dir(ctx_file_path)?;
+            save_json(ctx_file_path, &prev_prompt_ctx)?;
+
+            gen_req = gen_req.context(prev_prompt_ctx);
+        }
+
+        let final_data = gen_stream_print(&ollama, gen_req).await?;
+
+        if let Some(final_data) = final_data {
+            prev_prompt_ctx = Some(final_data.context);
+        }
+    }
 
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -16,7 +16,10 @@ pub mod consts {
 pub mod gen {
     use futures::StreamExt;
     use ollama_rs::{
-        generation::completion::request::GenerationRequest, Ollama
+        generation::completion::{
+            request::GenerationRequest, GenerationFinalResponseData
+        },
+        Ollama
     };
     use tokio::io::AsyncWriteExt;
 
@@ -25,32 +28,41 @@ pub mod gen {
     pub async fn gen_stream_print(
         ollama: &Ollama,
         gen_req: GenerationRequest
-    ) -> Result<()> {
+    ) -> Result<Option<GenerationFinalResponseData>> {
         let mut stream = ollama.generate_stream(gen_req).await?;
 
         let mut stdout = tokio::io::stdout();
-        let mut char_count = 0;
+        let mut line_words_len_count = 0;
 
         while let Some(res) = stream.next().await {
             let responses = res.unwrap();
 
-            for resp in responses {
-                let bytes = resp.response.as_bytes();
-
-                char_count += bytes.len();
-                if char_count > 80 {
+            for prompt_response in responses {
+                // early return if response is done
+                if let Some(final_data) = prompt_response.final_data {
                     stdout.write_all(b"\n").await?;
-                    char_count = 0;
+                    stdout.flush().await?;
+                    return Ok(Some(final_data));
                 }
 
-                // write output
-                stdout.write_all(bytes).await?;
+                // bytes will have 1 word step by step
+                let word_in_bytes = prompt_response.response.as_bytes();
+
+                // If line is already long, start a new line
+                line_words_len_count += word_in_bytes.len();
+                if line_words_len_count > 80 {
+                    stdout.write_all(b"\n").await?;
+                    line_words_len_count = 0;
+                }
+
+                // write line
+                stdout.write_all(word_in_bytes).await?;
                 stdout.flush().await?;
             }
         }
 
         stdout.write_all(b"\n").await?;
 
-        Ok(())
+        Ok(None)
     }
 }
