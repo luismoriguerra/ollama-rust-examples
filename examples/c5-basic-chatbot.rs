8570321bce015f9d41b4014f9e8ff1baa906dd32
Luis Mori Guerra <luismorith@gmail.com>
2024-04-22 19:09:18 -0500

    chat with inputs
    Signed-off-by: Luis Mori Guerra <luismorith@gmail.com>

---
+++ examples/c5-basic-chatbot.rs
@@ -0,0 +1,51 @@
+use futures::StreamExt;
+use ollama_rs::{
+    generation::completion::{
+        request::GenerationRequest, GenerationContext, GenerationResponseStream
+    },
+    Ollama
+};
+use tokio::io::{stdout, AsyncWriteExt};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let ollama = Ollama::default();
+
+    let mut stdout = stdout();
+
+    let mut context: Option<GenerationContext> = None;
+
+    loop {
+        stdout.write_all(b"\n> ").await?;
+        stdout.flush().await?;
+
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input)?;
+
+        let input = input.trim_end();
+        if input.eq_ignore_ascii_case("exit") {
+            break;
+        }
+
+        let mut request =
+            GenerationRequest::new("llama2:latest".into(), input.to_string());
+        if let Some(context) = context.clone() {
+            request = request.context(context);
+        }
+        let mut stream: GenerationResponseStream =
+            ollama.generate_stream(request).await?;
+
+        while let Some(Ok(res)) = stream.next().await {
+            for ele in res {
+                stdout.write_all(ele.response.as_bytes()).await?;
+                stdout.flush().await?;
+
+                if let Some(final_data) = ele.final_data {
+                    context = Some(final_data.context);
+                }
+            }
+        }
+    }
+
+    Ok(())
+}
---
+++ readme.md
@@ -0,0 +1,6 @@
+
+
+## Tutorial how to use ollama with rust 
+
+https://github.com/pepperoni21/ollama-rs/blob/HEAD/examples/basic_chatbot.rs
+
